51f5ab5dd8fb889a048123ed0846798a26644a4f
wogapat <59763871+wogapat@users.noreply.github.com>
2022-12-11 20:49:56 +0000

    v1.2.8

--- WogaLWRF/Devices/WogaLWRF - Lightwave Switch.groovy
+++ WogaLWRF/Devices/WogaLWRF - Lightwave Switch.groovy
@@ -15,39 +15,81 @@
  */
 metadata {
 	definition (name: "WogaLWRF - Lightwave Switch", namespace: "wogapat", author: "Patrick Wogan") {
-		capability "Switch"
-        
+		capability "Sensor"
+        capability "Contact Sensor"
+        capability "Switch"
+
+	 	command "open"
+		command "close"
+ 
+    }
+	preferences {
+        input name: "reversed", type: "bool", title: "Reverse Action"
+	}
+}
+
+def open(){
+	sendEvent(name: "contact", value: "open")
+	if(reversed) switchVal = "off"
+	else switchVal = "on"
+	sendEvent(name: "switch", value: switchVal)
+}
+
+def close(){
+	sendEvent(name: "contact", value: "closed")
+	if(reversed) switchVal = "on"
+	else switchVal = "off"
+	sendEvent(name: "switch", value: switchVal)
+}
+
+def on(){
+    sendEvent(name: "switch", value: "on")
+    if(reversed==true) {
+        contactVal = "closed"
+        parent?.setChildSwitch(device.deviceNetworkId, 0)
+    } else {
+        contactVal = "open"
+        parent?.setChildSwitch(device.deviceNetworkId, 1)
+    }
+	sendEvent(name: "contact", value: contactVal)
+}
+
+def off(){
+    sendEvent(name: "switch", value: "off")
+    if(reversed==true) {
+        contactVal = "open"
+        parent?.setChildSwitch(device.deviceNetworkId, 1)
+    } else {
+        contactVal = "closed"
+        parent?.setChildSwitch(device.deviceNetworkId, 0)
+    }
+	sendEvent(name: "contact", value: contactVal)
+}
+
+def poll() {
+    parent?.childPoll(device.deviceNetworkId)
+}
+
+private def myRunIn(delay_s, func) {
+    log.debug("myRunIn(${delay_s},${func})")
+
+	if (delay_s > 0) {
+        def tms = now() + (delay_s * 1000)
+        def date = new Date(tms)
+        runOnce(date, func)
+        //LOGDEBUG("'${func}' scheduled to run at ${date}")
     }
 }
 
-
-def on() {
-	parent?.setChildSwitchOn(device.deviceNetworkId)
-	}
-	
-def off() {
-	parent?.setChildSwitchOff(device.deviceNetworkId)
-	}
-	
-def updated() {
-	log.debug "updated()"
+def installed(){
 	initialize()
 }
 
-def uninstalled() {
-	log.debug "uninstalled()"
-	unschedule()
+def updated(){
+	initialize()
 }
 
-def initialize() {
-	unschedule()
-		}
-
-def parse(String description) {
-    description.each {
-        if (it.name in ["switch","level"]) {
-            if (txtEnable) log.info it.descriptionText
-            sendEvent(it)
-        }
-    }
+def initialize(){
+	sendEvent(name: "switch", value: "off")
+	sendEvent(name: "contact", value: "closed")
 }
