b917f364c5e0dce7dfa7dffc4ccf72ca0b3b24d9
wogapat <59763871+wogapat@users.noreply.github.com>
2022-12-11 20:27:07 +0000

    v1.2.7

--- WogaLWRF/Devices/WogaLWRF - L42.groovy
+++ WogaLWRF/Devices/WogaLWRF - L42.groovy
@@ -1,9 +1,7 @@
 /*
- *  WogaLWRF - L42
+ *  WogaLWRF - Lightwave Automation Device
  *
  *  Copyright 2021 Patrick Wogan
- *  
- *  Adapted From Virtual Contact Sensor with Switch by Stephan Hackett
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -16,119 +14,49 @@
  *
  */
 metadata {
-	definition (name: "WogaLWRF - L42", namespace: "wogapat", author: "Patrick Wogan") {
-		capability "Sensor"
-        capability "Contact Sensor"
-        capability "Switch"
-		capability "PowerMeter"
-		capability "EnergyMeter"
-		capability "ColorMode"
-		capability "VoltageMeasurement"
+	definition (name: "WogaLWRF - Lightwave Automation Device", namespace: "wogapat", author: "Patrick Wogan") {
+		capability "Switch"
         
-        	
-		command "open"
-		command "close"
-        command "ident"
-        command "childLock"
-        command "childUnlock"
-
-		attribute "identify", "String"
-		attribute "outletInUse", "bool"
-        attribute "childProtection", "String"
-
     }
-	preferences {
-        input name: "reversed", type: "bool", title: "Reverse Action"
-	}
 }
 
-def open(){
-	sendEvent(name: "contact", value: "open")
-	if(reversed) switchVal = "off"
-	else switchVal = "on"
-	sendEvent(name: "switch", value: switchVal)
-}
-
-def close(){
-	sendEvent(name: "contact", value: "closed")
-	if(reversed) switchVal = "on"
-	else switchVal = "off"
-	sendEvent(name: "switch", value: switchVal)
-}
-
-def on(){
+def on() {
+	parent?.setAutoDeviceOn(device.deviceNetworkId)
     sendEvent(name: "switch", value: "on")
-    if(reversed==true) {
-        contactVal = "closed"
-        parent?.setChildSwitch(device.deviceNetworkId, 0)
-    } else {
-        contactVal = "open"
-        parent?.setChildSwitch(device.deviceNetworkId, 1)
-    }
-	sendEvent(name: "contact", value: contactVal)
-}
-
-def off(){
+    myRunIn(2, "off")
+	}
+	
+def off() {
     sendEvent(name: "switch", value: "off")
-    if(reversed==true) {
-        contactVal = "open"
-        parent?.setChildSwitch(device.deviceNetworkId, 1)
-    } else {
-        contactVal = "closed"
-        parent?.setChildSwitch(device.deviceNetworkId, 0)
+	}
+	
+def updated() {
+	log.debug "updated()"
+	initialize()
+}
+
+def uninstalled() {
+	log.debug "uninstalled()"
+	unschedule()
+}
+
+def initialize() {
+	unschedule()
+		}
+
+def parse(String description) {
+    description.each {
+        if (it.name in ["switch","level"]) {
+            if (txtEnable) log.info it.descriptionText
+            sendEvent(it)
+        }
     }
-	sendEvent(name: "contact", value: contactVal)
-}
-
-
-def childLock() {
-    sendEvent(name: "childProtection", value: "on")
-	parent?.setChildProtection(device.deviceNetworkId, 1)
-}
-
-def childUnlock() {
-    sendEvent(name: "childProtection", value: "off")
-	parent?.setChildProtection(device.deviceNetworkId, 0)
-}
-
-def ident() {
-    sendEvent(name: "identify", value: "on")
-	parent?.setChildIdentify(device.deviceNetworkId, 1)
-	myRunIn(90, "disableIdentify")
-}
-
-private def disableIdentify() {
-    sendEvent(name: "identify", value: "off")
-	parent?.setChildIdentify(device.deviceNetworkId, 0)
-}
-
-def poll() {
-    parent?.childPoll(device.deviceNetworkId)
 }
 
 private def myRunIn(delay_s, func) {
-    log.debug("myRunIn(${delay_s},${func})")
-
-	if (delay_s > 0) {
+    if (delay_s > 0) {
         def tms = now() + (delay_s * 1000)
         def date = new Date(tms)
         runOnce(date, func)
-        //LOGDEBUG("'${func}' scheduled to run at ${date}")
     }
 }
-
-
-def installed(){
-	initialize()
-}
-
-def updated(){
-	initialize()
-}
-
-def initialize(){
-	sendEvent(name: "switch", value: "off")
-	sendEvent(name: "contact", value: "closed")
-}
-
-
